3dcbbc6f17912fd97777fbfcac4187a5054a5e8b
Emil Nazyrov <72194705+EmilNazyrov@users.noreply.github.com>
2023-04-13 01:53:33 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -1,11 +1,9 @@
 import pandas as pd
 import numpy as np
+import random
 
 
 chat_id = 702381553 # Ваш chat ID, не меняйте название переменной
 
-def solution(...) -> bool: # Одна или две выборке на входе, заполняется исходя из условия
-    # Измените код этой функции
-    # Это будет вашим решением
-    # Не меняйте название функции и её аргументы
-    return ... # Ваш ответ, True или False
+def solution(*args) -> bool: # Одна или две выборке на входе, заполняется исходя из условия
+    return random.choice([True, False])
